a8b2b145baae7b7436de21a0147a1d8b3ee6ae66
Ubuntu <ubuntu@ip-10-112-47-86.ec2.internal>
2013-05-27 11:10:54 +0000

    Changes for mapreduce and jprofile related changes

--- bin/env.sh
+++ bin/env.sh
@@ -13,6 +13,7 @@ S3_BUCKET=${S3_BUCKET:-intel-hadoop-bundles-$REGION}
 S3_ACCOUNT=${S3_ACCOUNT:-$AWS_ACCOUNT_ID}
 
 # The version of HBase to use and the distribution tarball location
+
 #export HBASE_VERSION=0.94.5
 #export HBASE_URL=http://www.apache.org/dist/hbase/hbase-$HBASE_VERSION/hbase-$HBASE_VERSION-security.tar.gz
 
@@ -24,10 +25,9 @@ export HBASE_URL=https://s3.amazonaws.com/Ram_bucket_us-east-1/hbase-0.97-SNAPSH
 # The version of Hadoop to use and the distribution tarball location
 
 #export HADOOP_VERSION=1.0.4
-#export HADOOP_URL=http://www.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION-bin.tar.gz
 export HADOOP_VERSION=2.0.3-alpha
 export HADOOP_URL=http://www.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
-
+#export HADOOP_URL=http://www.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION-bin.tar.gz
 
 ############################################################################
 # Generally, users do not need to edit below
@@ -45,7 +45,7 @@ REQUEST_TIMEOUT=300    # 5 minutes
 # Global tool options
 TOOL_OPTS=`echo -K "$EC2_PRIVATE_KEY" -C "$EC2_CERT" --request-timeout $REQUEST_TIMEOUT`
 
-BASE_AMI_VERSION="amzn-ami-pv-2013.03.rc-0.x86_64-s3"
+BASE_AMI_VERSION="amzn-ami-pv-2013.03.1.x86_64-s3"
 
 EPEL_RPM=http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm
 
